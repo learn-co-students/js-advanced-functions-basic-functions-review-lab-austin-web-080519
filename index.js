0fedc50f4984ef164c1416e94ff7019872a0a67b
Anais Ahmed <anaismahmed@uchicago.edu>
2019-09-23 15:36:30 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Your code here
+function saturdayFun(fun = 'roller-skate'){
+    return `This Saturday, I want to ${fun}!`;
+}
+let mondayWork = function(work = 'go to the office'){
+    return `This Monday, I will ${work}.`;
+}
+
+function wrapAdjective(wrapper = '*'){
+    return function(adj = 'special'){
+        return `You are ${wrapper}${adj}${wrapper}!`
+    }
+}
+
+const Calculator = {};
+Calculator['add'] = function(a, b){
+    return a+b;
+}
+Calculator['subtract'] = function(a, b){
+    return a-b;
+}
+Calculator['multiply'] = function(a, b){
+    return a*b;
+}
+Calculator['divide'] = function(a, b){
+    return a/b;
+}
+
+function actionApplyer(start, arr){
+    let total = start;
+    if(arr.length !== 0){
+        for(const func of arr) {
+            total = func(total);
+        }
+    }
+    return total;
+}
